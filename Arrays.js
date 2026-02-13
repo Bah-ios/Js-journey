dcc30a51636fce7b30dc0f788324171452039501
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-02-13 14:43:36 +0300

    Oprations on array

--- Arrays.js
+++ Arrays.js
@@ -1,10 +1,35 @@
-const numbers = [ 2,3,4,5,6];
+// const numbers = [ 2,3,4,5,6];
 
-console.log(numbers.indexOf(4));
-numbers.push(4,3)
-console.log(numbers);
-numbers.pop();
-console.log(numbers);
-console.log(numbers.lastIndexOf(4,2))
-console.log(numbers.lastIndexOf(4,3))
-numbers.includes(6);
+// console.log(numbers.indexOf(4));
+// numbers.push(4,3)
+// console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
+// console.log(numbers.lastIndexOf(4,2))
+// console.log(numbers.lastIndexOf(4,3))
+// numbers.includes(6);
+const rooms = [
+    {roomNumber:1, type:"sweet", isbooked:true },
+    {roomNumber:2, type:"presidential", isbooked:false} ,
+    {roomNumber:3, type:"standard", isbooked:false} 
+
+]
+
+rooms.splice(1,0,{name:"abebe",age:22})
+// const room = rooms.find(function(room){
+//     return room.type === "presidential"
+// })
+// console.log(room)
+
+const roomPosition = rooms.findIndex(function(room){
+    return room.type === "presidential"
+})
+console.log(roomPosition)
+
+const room = rooms.find (room => room.type === "presidential")
+const room1 = rooms.find(function(room){
+    return room.isbooked === true 
+})
+
+console.log(room)
+console.log(room1)
