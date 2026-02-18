3252c6d236dfa7f620b4c2ef07b50e8b00244c82
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-02-18 13:57:15 +0300

    array oprations

--- Arrays.js
+++ Arrays.js
@@ -8,28 +8,45 @@
 // console.log(numbers.lastIndexOf(4,2))
 // console.log(numbers.lastIndexOf(4,3))
 // numbers.includes(6);
-const rooms = [
-    {roomNumber:1, type:"sweet", isbooked:true },
-    {roomNumber:2, type:"presidential", isbooked:false} ,
-    {roomNumber:3, type:"standard", isbooked:false} 
+// const rooms = [
+//     {roomNumber:1, type:"sweet", isbooked:true },
+//     {roomNumber:2, type:"presidential", isbooked:false} ,
+//     {roomNumber:3, type:"standard", isbooked:false} 
 
-]
+// ]
 
-rooms.splice(1,0,{name:"abebe",age:22})
-// const room = rooms.find(function(room){
+// rooms.splice(1,0,{name:"abebe",age:22})
+// // const room = rooms.find(function(room){
+// //     return room.type === "presidential"
+// // })
+// // console.log(room)
+
+// const roomPosition = rooms.findIndex(function(room){
 //     return room.type === "presidential"
 // })
+// console.log(roomPosition)
+
+// const room = rooms.find (room => room.type === "presidential")
+// const room1 = rooms.find(function(room){
+//     return room.isbooked === true 
+// })
+
 // console.log(room)
+// console.log(room1)
 
-const roomPosition = rooms.findIndex(function(room){
-    return room.type === "presidential"
+const section1 = ['abebe', "kebede", "beti"]
+const section2 = ["mahlet","beti","mita"]
+
+// const combined = section1.concat(section2)
+
+// console.log(combined)
+
+const comb = [...section1, ...section2]
+console.log(comb)
+
+
+/////iterating through array
+comb.forEach(function(students){
+    console.log(students)
 })
-console.log(roomPosition)
 
-const room = rooms.find (room => room.type === "presidential")
-const room1 = rooms.find(function(room){
-    return room.isbooked === true 
-})
-
-console.log(room)
-console.log(room1)
