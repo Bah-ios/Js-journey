3f4f627bdf2395590c741821c780cf969a094fc5
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-02-20 10:39:39 +0300

    array sort

--- Arrays.js
+++ Arrays.js
@@ -34,26 +34,72 @@
 // console.log(room)
 // console.log(room1)
 
-const section1 = ['abebe', "kebede", "beti"]
-const section2 = ["mahlet","beti","mita"]
+// const section1 = ['abebe', "kebede", "beti"]
+// const section2 = ["mahlet","beti","mita"]
 
 // const combined = section1.concat(section2)
 
 // console.log(combined)
 
-const comb = [...section1, ...section2]
-// console.log(comb)
+// const combinedCopy = combined.slice(2, combined.length)
+// const combinedString = combined.join(",")
+
+// const combined2 = [...section1, '1',"2", ...section2]
+
+// for (let key of combined2){
+//     console.log(key)                            //iterating through array using for of loop
+// }
 
 
-// /////iterating through array
-// comb.forEach(function(students){
-//     console.log(students)
+// combined2.forEach(function(student){
+//    console.log(student)                       //iterating through array using forEach
 // })
 
-const finder = comb.find(function(student){
-   return student === "beti"
-        
-})
 
-console.log(finder)
+// combined2.forEach(person => console.log(person))
+// console.log(combined2)                                //Shorter way of forEach using arrow function
+
+// let numbers = [1,2,3,4,5]
+// const joined = numbers.join(",")
+// console.log(joined)
+
+// let message  = "this is the message from me to you saussage" 
+// const splitting = message.split(" ")                //splitting string into an array using split method
+// console.log(splitting)
+
+// const joining = splitting.join(" ")
+// console.log(joining)                    //joining array elements into a string    
+
+
+const cars = [
+    { name: "toyota", year: 1999, color : "red" },
+    { name : "nissan", year : 2023, color : "blue" },
+    { name : "Honda" , year : 2022 , color : "gray" },
+    { name : "bYD" , year : 2025, color : "brown" }
+]
+
+cars.sort(function(a,b){
+    const LetterofA = a.name.toUpperCase();
+    const LetterofB = b.name.toUpperCase();
+
+    
+    if (LetterofA < LetterofB ) return -1;
+    if (LetterofA > LetterofB ) return 1;
+    return 0
+})
+console.log(cars)
+
+
+
+
+
+
+
+// const finder = comb.find(function(student){
+//    return student === "beti"
+        
+// })
+
+
+// console.log(finder)
 
