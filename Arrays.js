24e4d80e4fa82ca8289ab6b43bcac9a8936b8d1d
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-02-27 09:49:46 +0300

    exercise

--- Arrays.js
+++ Arrays.js
@@ -71,81 +71,118 @@
 // console.log(joining)                    //joining array elements into a string    
 
 
-const cars = [
-    { name: "toyota", year: 1999, color : "red" },
-    { name : "nissan", year : 2023, color : "blue" },
-    { name : "Honda" , year : 2022 , color : "gray" },
-    { name : "bYD" , year : 2025, color : "brown" }
-]
+// const cars = [
+//     { name: "toyota", year: 1999, color : "red" },
+//     { name : "nissan", year : 2023, color : "blue" },
+//     { name : "Honda" , year : 2022 , color : "gray" },
+//     { name : "bYD" , year : 2025, color : "brown" }
+// ]
 
-cars.sort(function(a,b){
-    const LetterofA = a.name.toUpperCase();
-    const LetterofB = b.name.toUpperCase();               //sorting reference type elements of an array
+// cars.sort(function(a,b){
+//     const LetterofA = a.name.toUpperCase();
+//     const LetterofB = b.name.toUpperCase();               //sorting reference type elements of an array
 
     
-    if (LetterofA < LetterofB ) return -1;
-    if (LetterofA > LetterofB ) return 1;
-    return 0
-})
-console.log(cars)
+//     if (LetterofA < LetterofB ) return -1;
+//     if (LetterofA > LetterofB ) return 1;
+//     return 0
+// })
+// console.log(cars)
 
 
 
-const together = section1.concat(section2);
+// const together = section1.concat(section2);
 
-const studentname = "alem";
+// const studentname = "alem";
 
-const finder = together.find((student)=>{
-   return student === studentname;                 //element search in array 
-})
+// const finder = together.find((student)=>{
+//    return student === studentname;                 //element search in array 
+// })
  
-if (finder === studentname ){
-    console.log(true);
-}
-else console.log(false);
-console.log(together.includes(studentname));
+// if (finder === studentname ){
+//     console.log(true);
+// }
+// else console.log(false);
+// console.log(together.includes(studentname));
 
 
 
-const numbers = [ 1,3,6,2,8,-1,4,5,7,9]
-const checked = numbers.every(function(numberr){
-     return numberr >= 0
-    })
+// const numbers = [ 1,3,6,2,8,-1,4,5,7,9]
+// const checked = numbers.every(function(numberr){
+//      return numberr >= 0                                   //testing elements of array with every and some
+//     })
 
-const atLeastOne = numbers.some((number)=> number < 0)
+// const atLeastOne = numbers.some((number)=> number < 0)
 
-console.log(atLeastOne);
-console.log(numbers);
-const sorted = numbers.sort();
-console.log(sorted);
-const filtered = numbers.filter(function(number){
-    return number >4;
-})
-console.log(filtered);
+// console.log(atLeastOne);
+// console.log(numbers);                                    //array testing with some and sorting method
+// const sorted = numbers.sort();
+// console.log(sorted);
 
-const mapping = filtered.map(function(values){
-   return "<li>" + values + "</li>"
-})
-mapping.join()
-console.log(mapping)
 
-const Mapping_on_obj = filtered.map(function(elements){
 
-    const obj = { value : elements}
-    console.log(obj)
+// const filtered = numbers.filter(function(number){                
+//     return number >4;
+// })
+// console.log(filtered);
 
-})
-const array = [];          
-function rangeGenerator(min,max){
-    range = max - min;
+// const mapping = filtered.map(function(values){
+//    return "<li>" + values + "</li>"                   //array filtering and mapping  and also method chaining
+// })                                                              
+// mapping.join()
+// console.log(mapping)
+
+// const Mapping_on_obj = filtered.map(function(elements){
+
+//     const obj = { value : elements}
+//     console.log(obj)
+
+// })
+
+
+///range generator exercise 
+
+// const array = [];          
+// function rangeGenerator(min,max){
+//     range = max - min;
     
-    for (let i = 0; i<= range; i++ ){
-        array.push(min)
-        min++;
-    }
-    return array;                            
+//     for (let i = 0; i<= range; i++ ){
+//         array.push(min)
+//         min++;
+//     }
+//     return array;                            
+    
+// }
+// const printer = rangeGenerator(-1,3)
+// console.log(printer);
+
+
+
+///custom made inclides() method```
+
+// const array = [2, 4, 5, 7, 8]
+// function includess(arra , searchedNumber) {
+//     for (let i = 0; i <= arra.length; i++ )
+//         if (arra[i] === searchedNumber)
+//             return 
+//         else return true
+
+// }
+
+// const including = includess(array,9)
+// console.log(including)
+
+const numbers = [1, 4, 5, 6, 7, 9, 4,1,2,4]
+let exclude = [1,4]
+function execpt (array, exclude){
+    const excluded = []
+    for(let value of array){
+        if(exclude.includes(value))
+            excluded.push(value);
     
 }
-const printer = rangeGenerator(-1,3)
-console.log(printer);
+    return excluded;
+}
+const output = execpt(numbers, exclude)
 
+console.log(output)
