ce6bbd0e64f87bec32fcf3e3fda8a3e2f83dc7ee
bah-ios <bahran.solomon@astu.edu.et>
2026-09-15 02:29:15 -0400

    Guess the number game

---
+++ rand_number.js
@@ -0,0 +1,8 @@
+import * as readline from "node:readline/promises"
+import {stdin as input, stdout as output} from "node:process"
+
+const r1 = readline.createInterface({input, output})
+
+try{
+    const
+}
